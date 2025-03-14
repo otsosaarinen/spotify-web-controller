335645ba60f22c98c72317c79d6a1d11f9e88baa
Otso Saarinen <saarinenotso@gmail.com>
2025-03-14 13:07:23 +0200

    add button and welcome text

--- spotify-web-controller/src/Auth.tsx
+++ spotify-web-controller/src/Auth.tsx
@@ -1,3 +1,14 @@
-function Auth() {}
+import Button from "@mui/material/Button";
+
+function Auth() {
+    return (
+        <>
+            <div className="h-screen w-screen flex flex-col justify-center items-center">
+                <div>spotify-web-controller</div>
+                <Button size="large">Click here to authorize</Button>
+            </div>
+        </>
+    );
+}
 
 export default Auth;
