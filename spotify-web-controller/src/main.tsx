9d8d437a75de472a3b947d93dbb5f37c842e53db
Otso Saarinen <saarinenotso@gmail.com>
2025-03-15 12:24:13 +0200

    Update main.tsx

--- spotify-web-controller/src/main.tsx
+++ spotify-web-controller/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Auth from "./Auth.tsx";
+import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <Auth />
+        <App />
     </StrictMode>
 );
