fb2b663985021f3c34532528d819dca043725a90
Otso Saarinen <saarinenotso@gmail.com>
2025-03-15 12:24:22 +0200

    edit auth button

--- spotify-web-controller/src/Auth.tsx
+++ spotify-web-controller/src/Auth.tsx
@@ -3,9 +3,8 @@ import Button from "@mui/material/Button";
 function Auth() {
     return (
         <>
-            <div className="h-screen w-screen flex flex-col justify-center items-center">
-                <div>spotify-web-controller</div>
-                <Button size="large">Click here to authorize</Button>
+            <div className="">
+                <Button variant="contained">Click here to authorize</Button>
             </div>
         </>
     );
