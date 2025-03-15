adf946add689d2c0baf81b7481eba7eee9dc59f2
Otso Saarinen <saarinenotso@gmail.com>
2025-03-15 13:04:28 +0200

    fixed font import

--- spotify-web-controller/src/Header.tsx
+++ spotify-web-controller/src/Header.tsx
@@ -1,7 +1,7 @@
 function Header() {
     return (
         <>
-            <div className="fixed top-0 flex justify-center items-center bg-blue-400 h-auto w-full text-4xl p-[1rem]">
+            <div className="fixed top-0 flex justify-center items-center bg-blue-400 h-auto w-full text-4xl p-[1rem] font-roboto">
                 Spotify Web Controller
             </div>
         </>
