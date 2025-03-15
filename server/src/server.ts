85b34e1dc75fff098e623b6626c331210f87eb3f
otsosaarinen <saarinenotso@gmail.com>
2025-03-15 22:31:54 +0200

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,4 +4,13 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.post("/auth", (req, res) => {});
+app.use(express.json());
+
+app.post("/auth", (req, res) => {
+    console.log(req.body);
+    res.send(`POST request received at port ${port}`);
+});
+
+app.listen(port, () => {
+    console.log(`Express server running on port ${port}`);
+});
