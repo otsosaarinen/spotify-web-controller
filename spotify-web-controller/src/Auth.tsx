27cf091412371e7adac153d3beac829bb20b5afe
Otso Saarinen <saarinenotso@gmail.com>
2025-03-17 14:20:27 +0200

    Update Auth.tsx

--- spotify-web-controller/src/Auth.tsx
+++ spotify-web-controller/src/Auth.tsx
@@ -34,6 +34,10 @@ function Auth() {
             const verifier = generateRandomString(64);
             setCodeVerifier(verifier);
 
+            // save verifier to localstorage
+            localStorage.setItem("code_verifier", verifier);
+            console.log("This verifier saved to localstorage ", verifier);
+
             const hashed = await sha256(verifier);
             const challenge = base64encode(hashed);
             setCodeChallenge(challenge);
@@ -42,12 +46,65 @@ function Auth() {
         generateCodeChallenge();
     }, []);
 
+    const Authorization = () => {
+        const authUrl = new URL(
+            `http://localhost:3000/auth?code_challenge=${codeChallenge}`
+        );
+
+        window.location.href = authUrl.toString();
+    };
+
+    const getAccessToken = async (code: string) => {
+        const accessCodeVerifier = localStorage.getItem("code_verifier");
+        console.log(
+            "This verifier was loaded before request ",
+            accessCodeVerifier
+        );
+
+        if (!accessCodeVerifier) {
+            console.log("Code verifier is missing");
+            return;
+        }
+
+        try {
+            const response = await fetch(`http://localhost:3000/access`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    code,
+                    code_verifier: accessCodeVerifier,
+                }),
+            });
+
+            if (!response.ok) {
+                throw new Error("Failed to receive access token");
+            }
+
+            const data = await response.json();
+            console.log(data);
+        } catch (error) {
+            console.error("Error exchanging code for access token", error);
+        }
+    };
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(window.location.search);
+        let code = urlParams.get("code") as string;
+
+        if (!code) {
+            console.log("No code in the url, authorize using spotify first");
+        } else getAccessToken(code);
+    }, []);
+
     return (
         <div>
             <Button
                 variant="contained"
                 size="large"
                 endIcon={<OpenInNewIcon />}
+                onClick={Authorization}
             >
                 Click here to authorize
             </Button>
