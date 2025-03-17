bc404514aa747f70e923b9fd9aa9662b4cda78c4
Otso Saarinen <saarinenotso@gmail.com>
2025-03-17 14:39:07 +0200

    Update App.tsx

--- spotify-web-controller/src/App.tsx
+++ spotify-web-controller/src/App.tsx
@@ -1,12 +1,24 @@
+import { useState, useEffect } from "react";
 import Auth from "./Auth";
 import Header from "./Header";
 
 function App() {
+    const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
+    useEffect(() => {
+        const accessToken = localStorage.getItem("access_token");
+        if (accessToken) {
+            setIsAuthenticated(true);
+        }
+    }, []);
+
     return (
         <>
             <div className="flex justify-center items-center h-screen bg-white">
                 <Header />
-                <Auth />
+                <div>
+                    {isAuthenticated ? <p>You are authenticated</p> : <Auth />}
+                </div>
             </div>
         </>
     );
