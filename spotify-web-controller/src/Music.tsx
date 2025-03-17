22f90720f1dca904bf0120b65f50921f2896782c
Otso Saarinen <saarinenotso@gmail.com>
2025-03-17 16:00:53 +0200

    add skip song button

--- spotify-web-controller/src/Music.tsx
+++ spotify-web-controller/src/Music.tsx
@@ -2,9 +2,17 @@ import PlayCircleFilled from "@mui/icons-material/PlayCircleFilled";
 import { IconButton } from "@mui/material";
 
 function Music() {
+    const skipToNext = () => {
+        const access_token = localStorage.getItem("access_token");
+        fetch("https://api.spotify.com/v1/me/player/next", {
+            method: "POST",
+            headers: { Authorization: `Bearer ${access_token}` },
+        });
+    };
+
     return (
         <>
-            <IconButton size="large" color="primary">
+            <IconButton size="large" color="primary" onClick={skipToNext}>
                 <PlayCircleFilled fontSize="inherit" />
             </IconButton>
         </>
