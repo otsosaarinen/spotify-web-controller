6763ced80113f5feaeb2050d6c1d18803d3da730
Otso Saarinen <saarinenotso@gmail.com>
2025-03-17 15:49:26 +0200

    Update Music.tsx

--- spotify-web-controller/src/Music.tsx
+++ spotify-web-controller/src/Music.tsx
@@ -1,5 +1,14 @@
+import PlayCircleFilled from "@mui/icons-material/PlayCircleFilled";
+import { IconButton } from "@mui/material";
+
 function Music() {
-    return <></>;
+    return (
+        <>
+            <IconButton size="large" color="primary">
+                <PlayCircleFilled fontSize="inherit" />
+            </IconButton>
+        </>
+    );
 }
 
 export default Music;
