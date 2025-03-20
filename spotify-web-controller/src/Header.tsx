6d8cc7ba8f30116a74f7bc38d5e2e354932893d6
Otso Saarinen <saarinenotso@gmail.com>
2025-03-20 10:53:07 +0200

    Update Header.tsx

--- spotify-web-controller/src/Header.tsx
+++ spotify-web-controller/src/Header.tsx
@@ -1,7 +1,7 @@
 function Header() {
     return (
         <>
-            <div className="fixed top-0 flex justify-center items-center bg-blue-500 h-auto w-full text-4xl p-[1rem] font-roboto text-white">
+            <div className="fixed top-0 flex justify-center items-center bg-blue-600 h-auto w-full text-4xl p-[1rem] font-roboto text-white">
                 Spotify Web Controller
             </div>
         </>
