8a0928ec6e08fc494942efe2a2afd503f885e227
Otso Saarinen <saarinenotso@gmail.com>
2025-03-21 13:51:38 +0200

    removed font from css

--- spotify-web-controller/src/Header.tsx
+++ spotify-web-controller/src/Header.tsx
@@ -1,7 +1,7 @@
 function Header() {
     return (
         <>
-            <div className="fixed top-0 flex justify-center items-center bg-[#2196f3] h-auto w-full text-4xl p-[1rem] font-roboto text-white">
+            <div className="fixed top-0 flex justify-center items-center bg-[#2196f3] h-auto w-full text-4xl p-[1rem] text-white">
                 Spotify Web Controller
             </div>
         </>
